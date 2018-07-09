0e72b086326ee7babba7c780c5b38d2d5729d7b4
chencong <1085576236@qq.com>
2018-07-09 17:27:21 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# andlinks-cloud-sidecar
---
+++ src/test/java/com/andlinks/cloud/andlinkscloudsidecar/AndlinksCloudSidecarApplicationTests.java
@@ -0,0 +1,16 @@
+package com.andlinks.cloud.andlinkscloudsidecar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AndlinksCloudSidecarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
